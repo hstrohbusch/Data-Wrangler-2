7823c44957a258967a56dae68ef6072301c4882d
mgkukovec <62812450+mgkukovec@users.noreply.github.com>
2020-09-25 14:22:52 -0500

    Add files via upload

---
+++ DataWrangler.java
@@ -0,0 +1,68 @@
+// Name: Michael Kukovec
+// Email: kukovec@wisc.edu
+// Team: AD
+// TA: Sophie Stephenson
+// Lecturer: Gary Dahl
+// Notes to Grader: requires class BackEndHash to compile
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class DataWrangler {
+	public static boolean readInputFile(String file_name) throws FileNotFoundException {
+		//Title, Author, Publisher, Publication Year, ISBN
+		final int info_types = 5;
+		
+		//Open file
+		 try {
+		      File read_file = new File(file_name);
+		      Scanner scan = new Scanner(read_file);
+		      String line = scan.nextLine();
+		      
+		      while (scan.hasNextLine()) {
+		    	  String[] info = new String[info_types];
+		    	  for(int i = 0; i < info_types; i++) {
+		    		  info[i] = "";
+		    	  }
+		    	  line = scan.nextLine();
+		    	  
+		    	  //Create book objects with line data
+		    	  int i = 0;
+		    	  for(int count = 0; count < info_types; count++) {
+		    		  //checks for <","> delimiter
+		    		  if(line.charAt(i) == '\"') {
+		    			  i++;
+		    			  while(line.charAt(i) != '\"') {
+		    				  info[count] += line.charAt(i);
+		    				  i++;
+		    				  if(i > line.length() - 1) {
+		    					  break;
+		    				  }
+		    			  }
+		    			  i += 2;
+		    			  //Default delimiter is <,>
+		    		  } else {
+		    			  while(line.charAt(i) != ',') {
+		    				  info[count] += line.charAt(i);
+		    				  i++;
+		    				  if(i > line.length() - 1) {
+		    					  break;
+		    				  }
+		    			  }
+		    			  i++;
+		    		  }
+		    	  }
+		    	  //Create book
+		    	  Book newBook = new Book(info[0], info[1], info[2], Integer.parseInt(info[3]), info[4]);
+		    	  //Back-End function, adds Book to hash table
+		    	  //BackEndHash.add(newBook);
+		      }
+		      
+		      scan.close();
+		      return true;
+		    } catch (FileNotFoundException e) {
+		      return false;
+		    }
+	}
+}
